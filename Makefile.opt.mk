6ed422ba1d307ef9b10f173c0e93cd30fa2d21d9
cs224 <cs224@weisser-zwerg.dev>
2023-04-19 07:34:07 +0200

    Makefile.opt.mk

--- Makefile.opt.mk
+++ Makefile.opt.mk
@@ -3,14 +3,14 @@
 SHELL=/bin/bash
 
 setup:
-    mkdir -p /opt/weewx-wdc/data/duplicati &&\
-    mkdir -p /opt/weewx-wdc/data/weewx/data
-# mkdir -p /opt/weewx-wdc/data/weewx/public_html
-# mkdir -p /opt/weewx-wdc/data/weewx/archive
-# mkdir -p /opt/weewx-wdc/duplicati-backup
-# chown root:dialout /opt/weewx-wdc/data/weewx/data
-# chown root:dialout /opt/weewx-wdc/data/weewx/public_html
-# chown root:dialout /opt/weewx-wdc/data/weewx/archive
-# chmod g+w /opt/weewx-wdc/data/weewx/data
-# chmod g+w /opt/weewx-wdc/data/weewx/public_html
-# chmod g+w /opt/weewx-wdc/data/weewx/archive
+	mkdir -p /opt/weewx-wdc/data/duplicati &&\
+	mkdir -p /opt/weewx-wdc/data/weewx/data &&\
+	mkdir -p /opt/weewx-wdc/data/weewx/public_html &&\
+	mkdir -p /opt/weewx-wdc/data/weewx/archive &&\
+	mkdir -p /opt/weewx-wdc/duplicati-backup &&\
+	chown root:dialout /opt/weewx-wdc/data/weewx/data &&\
+	chown root:dialout /opt/weewx-wdc/data/weewx/public_html &&\
+	chown root:dialout /opt/weewx-wdc/data/weewx/archive &&\
+	chmod g+w /opt/weewx-wdc/data/weewx/data &&\
+	chmod g+w /opt/weewx-wdc/data/weewx/public_html &&\
+	chmod g+w /opt/weewx-wdc/data/weewx/archive &&\
